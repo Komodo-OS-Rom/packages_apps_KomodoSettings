4d1afb3cdd480b95b342a195b097ffc738efcbf0
Kry9toN <dhimasbagusprayoga@gmail.com>
2021-05-07 03:59:43 +0700

    ks: clean up battery fragment and change color icon

--- src/com/komodo/settings/fragments/StatusBarBattery.java
+++ src/com/komodo/settings/fragments/StatusBarBattery.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.komodo.settings;
+package com.komodo.settings.fragments;
 
 import android.content.ContentResolver;
 import android.content.Context;
